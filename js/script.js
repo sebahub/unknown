ab87e664d3f3fb89002bc50a3d785c884a2c99e4
sebahub <groovegeier@gmail.com>
2017-06-22 13:51:48 +0200

    Code
    codecode

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+//*** fixed on scroll ***//
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 66) {
+            $('nav').addClass('fixed');
+        }
+        else {
+            $('nav').removeClass('fixed');
+        }
+    });
+
+    var swiper = new Swiper('.swiper-container', {
+       pagination: '.swiper-pagination',
+       nextButton: '.swiper-button-next',
+       prevButton: '.swiper-button-prev',
+       slidesPerView: 1,
+       paginationClickable: true,
+       spaceBetween: 30,
+       loop: true
+   });
+
+$('.card_btn').on('click', function () {
+ $('.swiper-slide-active .card').toggleClass('flipped');
+ $(this).text(function(i, v){
+              return v === 'zurück' ? 'zum Rezept' : 'zurück';
+           });
+
+});
+
+$("input#spezialabo").click(function() {
+    $("input#ifchecked").attr("disabled", !this.checked);
+    $("input#later").prop('checked', false);
+});
+
+$("input#later").click(function() {
+    $("input#spezialabo").prop('checked', false);
+    $("input#ifchecked").attr("disabled", !this.unchecked);
+
+
+});
+
+
+});
